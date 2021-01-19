3efd84cc5bbfa4a56c7c136cbce4f4af9acfda0a
RanderGabriel <randergabriel98@gmail.com>
2021-01-18 23:43:38 -0300

    Adiciona React-Navigation

--- App.tsx
+++ App.tsx
@@ -1,13 +1,23 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import React, {FunctionComponent} from 'react';
 import {StatusBar} from 'react-native';
 import {Provider as PaperProvider} from 'react-native-paper';
 import {LoginScreen} from './app/modules/login/login-screen';
 
+const Stack = createStackNavigator();
+
 const App: FunctionComponent<any> = () => {
   return (
     <PaperProvider>
       <StatusBar barStyle="dark-content" />
-      <LoginScreen />
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{headerShown: false}}
+          initialRouteName="Login">
+          <Stack.Screen name="Login" component={LoginScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </PaperProvider>
   );
 };
