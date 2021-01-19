a3b9e7b4ecc68855d64ec69bb3caacba9197ce14
RanderGabriel <randergabriel98@gmail.com>
2021-01-18 23:47:39 -0300

    Ajustes de lint

--- app/components/atoms/button/button.tsx
+++ app/components/atoms/button/button.tsx
@@ -4,6 +4,8 @@ import {StyledButton} from './button.styles';
 
 const Button = (props: ButtonProps) => {
   return (
+    // @ts-ignore
+    // Devido a um problema na lib (https://github.com/callstack/react-native-paper/issues/1971)
     <StyledButton
       textColor={props.outline ? 'white' : '#6600cc'}
       mode={props.outline ? 'outlined' : 'contained'}
--- app/components/atoms/text-field/text-field.tsx
+++ app/components/atoms/text-field/text-field.tsx
@@ -4,6 +4,8 @@ import {TextFieldProps} from './text-field.props';
 
 export const TextField = (props: TextFieldProps) => {
   return (
+    // @ts-ignore
+    // Devido a um problema na lib (https://github.com/callstack/react-native-paper/issues/1971)
     <Input
       underlineColor="white"
       placeholderTextColor="white"
