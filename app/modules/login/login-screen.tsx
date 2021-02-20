a2931ba02e776da82f576bb834aba8969e05802a
Leonardo <oliveirabalsa2@gmail.com>
2021-02-20 11:06:00 -0300

    feat: add loading in login screen

--- app/modules/login/login-screen.tsx
+++ app/modules/login/login-screen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Alert } from 'react-native';
 import { Formik } from 'formik';
 import * as Yup from 'yup';
@@ -6,6 +6,7 @@ import { LoginForm } from '../../components/organisms/login-form/login-form';
 import { Container, StyledImage } from './login-screen.styles';
 import { Logo } from '../../components/atoms/logo/logo';
 import { useAuth } from '../../core/hooks/auth';
+import { ActivityIndicator, Colors } from 'react-native-paper';
 
 const SignupSchema = Yup.object().shape({
   password: Yup.string()
@@ -15,10 +16,18 @@ const SignupSchema = Yup.object().shape({
 });
 
 export const LoginScreen = () => {
+  const [loading, setLoading] = useState(false);
   const { signIn } = useAuth();
 
-  const onSubmit = (username: string, password: string) => {
-    signIn(username, password);
+  const onSubmit = async (username: string, password: string) => {
+    setLoading(true);
+    try {
+      await signIn(username, password);
+    } catch (err) {
+      Alert.alert('Erro ao logar este usuártio');
+    } finally {
+      setLoading(false);
+    }
   };
 
   const onSignup = () => {
@@ -27,30 +36,36 @@ export const LoginScreen = () => {
 
   return (
     <Container>
-      <StyledImage file={'paraisopolis'} />
-      <Logo />
-      <Formik
-        validationSchema={SignupSchema}
-        onSubmit={(values) => onSubmit(values.username, values.password)}
-        initialValues={{
-          username: '',
-          password: '',
-        }}>
-        {({ errors, handleChange, handleBlur, handleSubmit, values }) => (
-          <LoginForm
-            password={values.password}
-            username={values.username}
-            passwordError={errors.password}
-            usernameError={errors.username}
-            onPasswordBlur={handleBlur('password')}
-            onUsernameBlur={handleBlur('username')}
-            onPasswordChanged={handleChange('password')}
-            onUsernameChanged={handleChange('username')}
-            onSubmit={handleSubmit}
-            onSignup={onSignup}
-          />
-        )}
-      </Formik>
+      {loading ? (
+        <ActivityIndicator animating={true} color={'#fff'} size="large" />
+      ) : (
+        <>
+          <StyledImage file={'paraisopolis'} />
+          <Logo />
+          <Formik
+            validationSchema={SignupSchema}
+            onSubmit={(values) => onSubmit(values.username, values.password)}
+            initialValues={{
+              username: '',
+              password: '',
+            }}>
+            {({ errors, handleChange, handleBlur, handleSubmit, values }) => (
+              <LoginForm
+                password={values.password}
+                username={values.username}
+                passwordError={errors.password}
+                usernameError={errors.username}
+                onPasswordBlur={handleBlur('password')}
+                onUsernameBlur={handleBlur('username')}
+                onPasswordChanged={handleChange('password')}
+                onUsernameChanged={handleChange('username')}
+                onSubmit={handleSubmit}
+                onSignup={onSignup}
+              />
+            )}
+          </Formik>
+        </>
+      )}
     </Container>
   );
 };
