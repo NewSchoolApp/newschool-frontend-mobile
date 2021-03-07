e823d299940df75ccf46df3bfd8ed0dbadf1b352
Matheus Alves <ms.alvexx@gmail.com>
2021-03-06 22:42:52 -0300

    test: add useFetch tests

---
+++ tests/core/hooks/helpers.spec.ts
@@ -0,0 +1,39 @@
+import { renderHook, act } from '@testing-library/react-hooks'
+import { useFetch } from "@ns/core/hooks/helpers"
+
+const makeFakeFetch = () => {
+  return Promise.resolve('any_thing')
+}
+
+describe('helpers', () => {
+  describe('useFetch', () => {
+    test('Should set loading to true when fetch is called', async () => {
+      const sut = useFetch
+      const { result } = renderHook(() => sut(makeFakeFetch));  
+      const promise = act(() => result.current.fetch());
+      expect(result.current.loading).toBe(true);
+      await promise
+    })
+
+    test('Should set loading to false when fetch is resolved', async() => {
+      const sut = useFetch
+      const { result } = renderHook(() => sut(makeFakeFetch));
+      await act(() => result.current.fetch());
+      expect(result.current.loading).toBe(false);
+    })
+
+    test('Should return fetch result when succeeds', async() => {
+      const sut = useFetch
+      const { result } = renderHook(() => sut(makeFakeFetch));
+      await act(() => result.current.fetch());
+      expect(result.current.data).toBe('any_thing');
+    })
+
+    test('Should return fetch result when succeeds', async() => {
+      const sut = useFetch
+      const { result } = renderHook(() => sut(() => Promise.reject(new Error)));
+      await act(() => result.current.fetch());
+      expect(result.current.error).toBeDefined()
+    })
+  })
+})
