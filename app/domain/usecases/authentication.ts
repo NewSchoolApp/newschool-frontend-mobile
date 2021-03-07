ce349eede1f89c39ed2f0aa4e8843c725596f051
Matheus Alves <ms.alvexx@gmail.com>
2021-03-06 23:35:45 -0300

    feat: add authentication usecase

---
+++ app/domain/usecases/authentication.ts
@@ -0,0 +1,14 @@
+export interface Authentication {
+  signIn: (data: Authentication.Params) => Promise<Authentication.Result>
+}
+
+export namespace Authentication {
+  export type Params = {
+    username: string;
+    password: string;
+  }
+
+  export type Result = {
+    token: string;
+  } | null
+}
