98413f5ec92a7758d0f4bd24e2d60e0ae80154db
Matheus Alves <ms.alvexx@gmail.com>
2021-03-06 23:37:02 -0300

    test: ensure RemoteAuthentication calls HttpPostClient with correct values

---
+++ tests/data/usecases/remote-authentication.spec.ts
@@ -0,0 +1,38 @@
+import { HttpPostClient } from "@ns/data/protocols/http-post-client"
+import { RemoteAuthentication } from "@ns/data/usecases/remote-authentication"
+
+class HttpPostClientSpy implements HttpPostClient {
+  params: any = null
+  result: any = null
+  async post <P extends any, R extends any>(data: P): Promise<R> {
+    this.params = data
+    this.result = Promise.resolve('any_value')
+    return this.result
+  } 
+}
+
+export type SutTypes = {
+  sut: RemoteAuthentication
+  httpPostClientSpy: HttpPostClientSpy
+}
+
+const makeSut = (): SutTypes => {
+  const httpPostClientSpy = new HttpPostClientSpy()
+  const sut = new RemoteAuthentication(httpPostClientSpy)
+  return {
+    sut, 
+    httpPostClientSpy
+  }
+}
+
+describe('Remote Authentication', () => {
+  test('Should call HttpPostClient with correct values', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    const data = {
+      username: 'any_username',
+      password: 'any_password'
+    }
+    await sut.signIn(data)
+    expect(httpPostClientSpy.params).toEqual(data)
+  })
+})
