c9f7718445773a0071259d3b7112b21331dbca69
Matheus Alves <ms.alvexx@gmail.com>
2021-03-11 22:57:52 -0300

    refactor: altera o factory do Authentication após as mudanças no HttpClient

--- app/main/factories/usecases/authentication.ts
+++ app/main/factories/usecases/authentication.ts
@@ -1,16 +1,15 @@
 import { RemoteAuthentication } from "@ns/data/usecases/remote-authentication";
 import { Authentication } from "@ns/domain/usecases/authentication";
 import { AxiosHttpClient } from "@ns/infra/axios/axios-http-client";
-import AxiosHelper from "@ns/data/protocols/http-helper";
+import HttpHelper from "@ns/data/protocols/http-helper";
 import env from "@ns/main/config/env";
 
 export const makeAuthentication = (): Authentication => {
-  const configurations = {
-    headers: {
-      'Authorization': AxiosHelper.getPasswordCredentials(),
-      'Content-Type': 'multipart/form-data',
-    },
+  const url = env.baseUrl + '/oauth/token'
+  const headers = {
+    'Authorization': HttpHelper.getPasswordCredentials(),
+    'Content-Type': 'multipart/form-data',
   }
-  const httpPostClient = new AxiosHttpClient(env.baseUrl + '/oauth/token', configurations)
-  return new RemoteAuthentication(httpPostClient)
+  const httpPostClient = new AxiosHttpClient()
+  return new RemoteAuthentication(httpPostClient, url, headers)
 }
