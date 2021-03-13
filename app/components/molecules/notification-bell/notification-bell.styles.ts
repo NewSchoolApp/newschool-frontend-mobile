3bd86c6bed88ca288017a15070b0f8b35dea6462
Danilo Silva <danilosoaresdasilva@hotmail.com>
2021-03-13 16:08:23 -0300

    refactor: removed commented lines

--- app/components/molecules/notification-bell/notification-bell.styles.ts
+++ app/components/molecules/notification-bell/notification-bell.styles.ts
@@ -20,26 +20,3 @@ export const StyledBell = styled.View`
   width: 54px;
   position: absolute;
 `;
-
-// export const Style = StyleSheet.create({
-//   badge:{
-//     width: 30,
-//     position: 'absolute',
-//     zIndex: 1
-//   },
-//   bell:{
-//     width: 54,
-//     position: 'absolute'
-
-//   },
-//   container:{
-//     margin: 10,
-//     alignContent: 'center',
-//     width: 54,
-//     height: 54,
-//     borderColor: 'black',
-//     borderStyle: 'solid',
-//     alignItems: 'flex-end'
-
-//   }
-// });
