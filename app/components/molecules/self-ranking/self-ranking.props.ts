ed5e6b2e7230bbb86d2b25c44cf155855427ea75
Danilo Silva <danilosoaresdasilva@hotmail.com>
2021-03-13 17:03:12 -0300

    feat: created self ranking component

---
+++ app/components/molecules/self-ranking/self-ranking.props.ts
@@ -0,0 +1,5 @@
+export interface SelfRankingProps {
+  rank: number;
+  points: number;
+  avatar: string;
+}
---
+++ app/components/molecules/self-ranking/self-ranking.style.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  width: 337px;
+  height: 119px;
+  border-radius: 8px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  left: 20px;
+`;
+export const Text = styled.Text`
+  font-family: 'Roboto';
+  font-size: 18px;
+  font-weight: 900;
+  line-height: 21px;
+  color: #6600cc;
+`;
+
+export const Image = styled.Image`
+  width: 111px;
+  height: 111px;
+  border-radius: 55px;
+`;
