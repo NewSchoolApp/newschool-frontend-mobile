18a6f4ac0d9e47e62e899a07c17dd85ad86d5ea5
Matheus Alves <ms.alvexx@gmail.com>
2021-03-27 12:13:47 -0300

    test: refactor test

--- tests/ui/organisms/bottom-tab.spec.tsx
+++ tests/ui/organisms/bottom-tab.spec.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { render } from '@testing-library/react-native';
+import { render, waitFor } from '@testing-library/react-native';
 import BottomTabs from '@ns/ui/components/organisms/bottom-tab/bottom-tab';
 
 describe('Bottom Tab', () => {
   test('Should start with home page rendered', () => {
     const { queryAllByTestId } = render(<BottomTabs />);
-    const homePageSelectors = queryAllByTestId('home');
+    const homePageSelectors = queryAllByTestId('home-btn');
+    waitFor(() => homePageSelectors);
     expect(homePageSelectors.length).toBe(1);
   });
 });
