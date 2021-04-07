df5aba6d4a73784b7c3ddc7834c19dcc850b4345
Danilo Silva <danilosoaresdasilva@hotmail.com>
2021-04-07 14:41:24 -0300

    fix: avatarType and medalType property problems on Lint

--- app/components/atom/podium-avatar/podium-avatar.style.ts
+++ app/components/atom/podium-avatar/podium-avatar.style.ts
@@ -1,12 +1,12 @@
 import styled from 'styled-components/native';
 
-export const AvatarContainer = styled.View`
+export const AvatarContainer = styled.View<{ avatarType: string }>`
   width: ${(props) => (props.avatarType == 'gold' ? '70px' : '58px')};
   height: ${(props) => (props.avatarType == 'gold' ? '70px' : '58px')};
   margin-bottom: ${(props) => (props.avatarType == 'gold' ? '5px' : '15px')};
 `;
 
-export const ImgAvatar = styled.Image`
+export const ImgAvatar = styled.Image<{ avatarType: string }>`
   position: relative;
   width: ${(props) => (props.avatarType == 'gold' ? '70px' : '58px')};
   height: ${(props) => (props.avatarType == 'gold' ? '70px' : '58px')};
--- app/components/atom/podium-medal/podium-medal.style.ts
+++ app/components/atom/podium-medal/podium-medal.style.ts
@@ -1,12 +1,12 @@
 import styled from 'styled-components/native';
 
-export const MedalContainer = styled.View`
+export const MedalContainer = styled.View<{ medalType: string }>`
   width: ${(props) => (props.medalType == 'gold' ? '35px' : '30px')};
   height: ${(props) => (props.medalType == 'gold' ? '35px' : '30px')};
   margin-bottom: ${(props) => (props.medalType == 'gold' ? '6px' : '10px')};
 `;
 
-export const ImgMedal = styled.Image`
+export const ImgMedal = styled.Image<{ medalType: string }>`
   position: relative;
   width: ${(props) => (props.medalType == 'gold' ? '35px' : '30px')};
   height: ${(props) => (props.medalType == 'gold' ? '35px' : '30px')};
