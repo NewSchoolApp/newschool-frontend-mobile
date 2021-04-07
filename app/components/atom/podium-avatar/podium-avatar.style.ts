3e50bba75b6f615c7c44c67ba8e36e6807c27b90
Danilo Silva <danilosoaresdasilva@hotmail.com>
2021-04-07 14:50:03 -0300

    fix: avatarType and medalType property problems on Lint. Importing interface

--- app/components/atom/podium-avatar/podium-avatar.style.ts
+++ app/components/atom/podium-avatar/podium-avatar.style.ts
@@ -1,12 +1,13 @@
 import styled from 'styled-components/native';
+import { PodiumAvatarProps } from './podium-avatar.props';
 
-export const AvatarContainer = styled.View<{ avatarType: string }>`
+export const AvatarContainer = styled.View<PodiumAvatarProps>`
   width: ${(props) => (props.avatarType == 'gold' ? '70px' : '58px')};
   height: ${(props) => (props.avatarType == 'gold' ? '70px' : '58px')};
   margin-bottom: ${(props) => (props.avatarType == 'gold' ? '5px' : '15px')};
 `;
 
-export const ImgAvatar = styled.Image<{ avatarType: string }>`
+export const ImgAvatar = styled.Image<PodiumAvatarProps>`
   position: relative;
   width: ${(props) => (props.avatarType == 'gold' ? '70px' : '58px')};
   height: ${(props) => (props.avatarType == 'gold' ? '70px' : '58px')};
--- app/components/atom/podium-medal/podium-medal.style.ts
+++ app/components/atom/podium-medal/podium-medal.style.ts
@@ -1,12 +1,13 @@
 import styled from 'styled-components/native';
+import { PodiumMedalProps } from './podium-medal.props';
 
-export const MedalContainer = styled.View<{ medalType: string }>`
+export const MedalContainer = styled.View<PodiumMedalProps>`
   width: ${(props) => (props.medalType == 'gold' ? '35px' : '30px')};
   height: ${(props) => (props.medalType == 'gold' ? '35px' : '30px')};
   margin-bottom: ${(props) => (props.medalType == 'gold' ? '6px' : '10px')};
 `;
 
-export const ImgMedal = styled.Image<{ medalType: string }>`
+export const ImgMedal = styled.Image<PodiumMedalProps>`
   position: relative;
   width: ${(props) => (props.medalType == 'gold' ? '35px' : '30px')};
   height: ${(props) => (props.medalType == 'gold' ? '35px' : '30px')};
